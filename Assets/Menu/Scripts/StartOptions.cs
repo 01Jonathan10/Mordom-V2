3ea4cda425ea50bd0728cd1814f41001d3bef709
01Jonathan10 <jonathansuper1@gmail.com>
2018-08-24 13:16:26 -0300

    Initial do loco

---
+++ Assets/Menu/Scripts/StartOptions.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+
+public class StartOptions : MonoBehaviour {
+
+	public bool changeMusicOnStart=true;
+
+	[HideInInspector] public bool inMainMenu = true;
+	[HideInInspector] public Animator animColorFade;
+	[HideInInspector] public Animator animMenuAlpha;
+	public AnimationClip fadeColorAnimationClip;
+	[HideInInspector] public AnimationClip fadeAlphaAnimationClip;
+
+	private PlayMusic playMusic;
+	private float fastFadeIn = .01f;
+	private ShowPanels showPanels;
+	
+	void Awake()
+	{
+		showPanels = GetComponent<ShowPanels> ();
+		playMusic = GetComponent<PlayMusic> ();
+	}
+
+
+	public void StartButtonClicked()
+	{
+		
+		playMusic.FadeDown(fadeColorAnimationClip.length);
+		Invoke ("LoadDelayed", fadeColorAnimationClip.length * .5f);
+		animColorFade.SetTrigger ("fade");
+
+	}
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += SceneWasLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= SceneWasLoaded;
+    }
+
+    void SceneWasLoaded(Scene scene, LoadSceneMode mode)
+    {
+		playMusic.PlayLevelMusic ();
+	}
+
+
+	public void LoadDelayed()
+	{
+		inMainMenu = false;
+		showPanels.HideMenu ();
+		SceneManager.LoadScene (1);
+	}
+
+	public void HideDelayed()
+	{
+		showPanels.HideMenu();
+	}
+
+	public void PlayNewMusic()
+	{
+		playMusic.FadeUp (fastFadeIn);
+		playMusic.PlaySelectedMusic (1);
+	}
+}
