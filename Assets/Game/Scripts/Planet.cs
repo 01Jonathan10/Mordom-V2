f8cade23784897dd8a49b190f4b86bffb48f8650
01Jonathan10 <jonathansuper1@gmail.com>
2018-08-31 12:56:18 -0300

    Initial

---
+++ Assets/Game/Scripts/Planet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+
+	public int life;
+	public int shield;
+	public GameObject currentWeapon;
+
+	// Use this for initialization
+	void Start () {
+		life = 100;
+		shield = 0;
+		currentWeapon = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Game/Scripts/Weapon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+	int id = 0;
+
+	void Shoot(){}
+}
+
+public class WeaponData : Object {
+
+}
